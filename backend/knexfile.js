a810e663f64bf8373e4ad9539f4d8263ef9f4746
Bruno R.Novais <novais.rezende@gmail.com>
2020-09-25 12:44:57 -0300

    Knex configured and migrations created

--- backend/knexfile.js
+++ backend/knexfile.js
@@ -2,43 +2,18 @@
 
 module.exports = {
 
-  development: {
-    client: 'sqlite3',
-    connection: {
-      filename: './dev.sqlite3'
-    }
+  client: 'postgresql',
+  connection: {
+    database: 'knowledge',
+    user: 'postgres',
+    password: 'password'
   },
-
-  staging: {
-    client: 'postgresql',
-    connection: {
-      database: 'my_db',
-      user:     'username',
-      password: 'password'
-    },
-    pool: {
-      min: 2,
-      max: 10
-    },
-    migrations: {
-      tableName: 'knex_migrations'
-    }
+  pool: {
+    min: 2,
+    max: 10
   },
-
-  production: {
-    client: 'postgresql',
-    connection: {
-      database: 'my_db',
-      user:     'username',
-      password: 'password'
-    },
-    pool: {
-      min: 2,
-      max: 10
-    },
-    migrations: {
-      tableName: 'knex_migrations'
-    }
+  migrations: {
+    tableName: 'knex_migrations'
   }
 
 };
